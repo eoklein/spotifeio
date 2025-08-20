f84593ced0300c3ac0d12f498d3fe3188c63e2dc
eoklein <otimosprimevascodagama@gmail.com>
2025-08-20 18:21:02 -0300

    nada

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -1,3 +1,20 @@
 export const enviroment = {
     production: false
 }
+
+export const SpotifyConfiguration = {
+    clientId: '57bbe48c63074e1ba1ca922f0f296697',
+    authEndpoint: 'https://accounts.spotify.com/authorize',
+    apiTokenEndpoint: 'https://accounts.spotify.com/api/token',
+    redirectUri: 'http://127.0.0.1:4200/login',
+    scopes: [
+        'user-read-currently-playing',
+        'user-read-playback-state',
+        'user-read-recently-played',
+        'user-top-read',
+        'user-modify-playback-state',
+        'user-library-read',
+        'playlist-read-private',
+        'playlist-read-collaborative',
+    ]
+}

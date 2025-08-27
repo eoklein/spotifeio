f1c9d843546834ccc1b4055cb95143f78ab956c9
eoklein <otimosprimevascodagama@gmail.com>
2025-08-26 22:19:56 -0300

    .

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,18 @@
-import { CanActivateFn, UrlTree } from "@angular/router";
+import { inject } from "@angular/core";
+import { CanActivateFn, Router, UrlTree } from "@angular/router";
 
 export const authGuard: CanActivateFn = async (rota, estado): Promise<boolean | UrlTree> => {
+
+    const roteador = inject(Router);
+    const access_token = localStorage.getItem('access_token');
+    if (!access_token) {
+        alert('Usuário não autenticado. Por favor, faça login.');
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('code_verifier');
+        return roteador.parseUrl('/login');
+    }
+
+
     return true;
+
 }
